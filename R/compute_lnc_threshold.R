d890fb733edc36b90966d06c7cf58f6bcb49bc59
Isaac <isaacmichaud@gmail.com>
2018-05-22 16:25:10 -0600

    Fixing Compilation issues
    got ride of fopenmp option that was throwing error with clang

--- R/compute_lnc_threshold.R
+++ R/compute_lnc_threshold.R
@@ -1,3 +1,16 @@
+#' Title
+#'
+#' @param d
+#' @param k
+#' @param cached
+#'
+#' @return
+#' @export
+#'
+#' @useDynLib rmi
+#' @importFrom Rcpp sourceCpp
+#'
+#' @examples
 compute_lnc_threshold <- function(d,k,cached=TRUE) {
  if ((d <= 100) & (k <= 100)) {
     return(rmi::alpha_thresholds[d,k])
